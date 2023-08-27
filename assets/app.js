acb8361f278fd158655de6f9911c5fbafc72c65b
KIUR <64315945+KIUR2223@users.noreply.github.com>
2023-08-27 15:04:27 +0800

    complete
    complete

--- assets/app.js
+++ assets/app.js
@@ -18,27 +18,23 @@ function scrollToTop() {
 }
 //Preloader
 setTimeout(function() {
-    // Hide the preloader
     document.querySelector('#preloader').style.display = 'none';
 
-    // Show the main content
     document.getElementById('mainContent').style.display = 'block';
-  }, 3000); // Delay in milliseconds (e.g., 3000ms = 3 seconds)
+  }, 3000);
 
 
 document.addEventListener("DOMContentLoaded", function() {
     const contactForm = document.getElementById("contact-form");
     contactMessage = document.getElementById("contact-message")
   
-    emailjs.init("01p3TuyY4jDH7U8Bk"); // Replace with your User ID from EmailJS
+    emailjs.init("01p3TuyY4jDH7U8Bk");
   
     contactForm.addEventListener("submit", function(event) {
       event.preventDefault();
   
-      // Collect form data
       const formData = new FormData(contactForm);
   
-      // Send email
       emailjs.sendForm("service_et1qean", "template_r67smws", "#contact-form")
         .then(() =>{
           contactMessage.textContent = "Message sent succesfully"
@@ -52,7 +48,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
         .catch(function(error) {
           console.error("Email failed to send:", error);
-          // Add code to show an error message to the user
         });
     });
     contactForm.reset()
